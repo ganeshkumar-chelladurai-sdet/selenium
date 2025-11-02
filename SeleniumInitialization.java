2f5ae91fdfda69df5fa39ba196ca6f81b00eb1a1
Ganeshkumar Chelladurai <ganeshkumar.chelladurai.sdet@gmail.com>
2025-11-02 11:59:54 +0000

    Enhance SeleniumInitialization with comments
    Added comments to clarify the purpose of each step in the Selenium initialization process.

--- SeleniumInitialization.java
+++ SeleniumInitialization.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumInitialization {
 
 	public static void main(String[] args) {
-		
+		//Declaring WebDriver and ChromeDriver - This will invoke the browser
 		WebDriver driver = new ChromeDriver();
+		
+		//This will get into URL
 		driver.get("https://rahulshettyacademy.com");
+		
+		//This will get the title of the site
 		System.out.println(driver.getTitle());
+		
+		//This will get the current URL
 		System.out.println(driver.getCurrentUrl());
 		driver.close();
 
